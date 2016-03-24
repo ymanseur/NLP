6083540c7302ccab3c7e37be73363fb69aca7b6c
Yacine Manseur <ymanseur94@gmail.com>
2016-03-24 18:21:28 -0400

    quick backup

--- project1/train.py
+++ project1/train.py
@@ -1,2 +1,21 @@
 # Yacine Manseur
 # train.py
+
+import argparse
+from nltk.tokenize import RegexpTokenizer
+
+def processArguments():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-l", "--trainingSet", help="path to labelled training set")
+	ap.add_argument("-o", "--testSet", help="parth to unlabelled test set")
+	args = vars(ap.parse_args())
+
+
+
+
+def main():
+	processArguments()
+
+
+if __name__ == "__main__":
+	main()
